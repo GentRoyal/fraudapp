b79a23e8e4ca7d8fe0e22521db0d214aac45f00f
Ridwan Yusuf <yusuflarnrey@gmail.com>
2025-02-15 12:49:46 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle as pkl
+import streamlit as st
+
+# Configure App
+st.set_page_config(
+	page_title = "Fraud Detection App",
+	page_icon="🛡️",
+    layout="wide"
+)
+
+# Load Model
+def load_model():
+    with open("best_model.pkl", 'rb') as file:
+        return pkl.load(file)
+        
+def generate_random_values():
+    random_values = {}
+    random_values['Time'] = np.random.uniform(0, 1000)
+    for i in range(1, 29):
+        random_values[f'V{i}'] = np.random.uniform(-10, 10)
+    
+    random_values['Amount'] = np.random.uniform(0, 1000000)
+    
+    return random_values
+    
+        
+def make_predictions():
+    st.title("Generate Prediction")
+    
+    if st.button("Generate Forecast"):
+        st.session_state.random_values = generate_random_values()
+        st.rerun()
